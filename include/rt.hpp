44c1eec63fc51d1a4eb888cde5e8f4b874876bab
ladevieq <ladevie.quentin@gmail.com>
2020-12-20 22:07:18 +0100

    Add random number utility functions

--- include/rt.hpp
+++ include/rt.hpp
@@ -2,6 +2,7 @@
 #define __RT_HPP_
 
 #include <limits>
+#include <cstdlib>
 
 const double pi = 3.1415926535897932385;
 const double infinity = std::numeric_limits<double>::infinity();
@@ -9,6 +10,15 @@ const double infinity = std::numeric_limits<double>::infinity();
 inline double deg_to_rad(double deg) {
     return deg * pi / 180.0;
 }
+
+inline double randd() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double randd(double min, double max) {
+    return min + randd() * (max - min);
+}
+
 
 #include "vec3.hpp"
 #include "ray.hpp"
